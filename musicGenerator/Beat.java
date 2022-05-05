2c6c7afef7b355f370e5a2ea1237b019a755ddba
Tom Gerambio <77496278+tgerambio@users.noreply.github.com>
2022-05-05 09:50:53 -0500

    Update Beat.java

--- musicGenerator/Beat.java
+++ musicGenerator/Beat.java
@@ -5,23 +5,31 @@ public class Beat {
 	public static int counter;
 	
 	public static void cut(int ms) {
-		try { Thread.sleep(ms); 
-	    } catch( InterruptedException e ) {
-	        e.printStackTrace();
-	    }
+		try {
+			Thread.sleep(ms); 
+	   	 } 
+		catch( InterruptedException e ) {
+	       	 	e.printStackTrace();
+	   	 }
 	}
+	
 	public static void swing(int ms) {
-		try { Thread.sleep(counter%2==0? ms : ms/2); 
-	    } catch( InterruptedException e ) {
-	        e.printStackTrace();
-	    }
+		try { 
+			Thread.sleep(counter%2==0? ms : ms/2); 
+	    	} 
+		catch( InterruptedException e ) {
+	        	e.printStackTrace();
+	    	}
 		counter++;
 	}
-	public static void swint(int ms, int latency) {
-		try { Thread.sleep(counter%2==0? ms : ms/latency); 
-	    } catch( InterruptedException e ) {
-	        e.printStackTrace();
-	    }
-		counter++;
+	
+	public static void swing(int ms, int latency) {
+		try { 	
+			Thread.sleep(counter%2==0? ms : ms/latency); 
+	    	} 
+		catch( InterruptedException e ) {
+	        	e.printStackTrace();
+	   	 }
+	         counter++;
 	}
 }
