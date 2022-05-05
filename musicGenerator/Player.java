37b18de8df19023533ac9d47bae1657db8c2c4ad
Tom Gerambio <77496278+tgerambio@users.noreply.github.com>
2022-05-05 10:28:01 -0500

    Update Player.java

--- musicGenerator/Player.java
+++ musicGenerator/Player.java
@@ -4,9 +4,11 @@ import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
+
+
 public class Player {
 
-	Synthesizer midiSynth;
+    Synthesizer midiSynth;
     Instrument[] instr; 
     MidiChannel[] mChannels; 
     
